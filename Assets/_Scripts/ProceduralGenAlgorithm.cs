cd469cf593b8b0ab16f72b3169d8ac44b5f0fe09
kovacskristofgabor <71077190+kovacskristofgabor@users.noreply.github.com>
2021-10-08 15:27:53 +0200

    Prog2Projekt Assett/Script
    2D Unity Procedurálisan Generált dungeon assett-ek, C# scriptek

---
+++ Assets/_Scripts/ProceduralGenAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProceduralGenAlgorithm
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousposition = startPosition;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousposition + Direction2D.GetRandomCardinalDirection();
+            path.Add(newPosition);
+            previousposition = newPosition;
+        }
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionsList = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), //FEL
+        new Vector2Int(1,0), //JOBB
+        new Vector2Int(0,-1), //LE
+        new Vector2Int(-1,0) //BAL
+    };
+
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+    }
+}
